0eee1358b454d66350628b8c60d4a1b03c91d499
Geoff <geoffreylaporte@gmail.com>
2020-12-11 10:39:53 -0600

    added gif for greenhouse

--- src/screens/GreenHouse/Greenhouse.jsx
+++ src/screens/GreenHouse/Greenhouse.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Layout from '../../components/shared/Layout/Layout';
 import './Greenhouse.css'
-import GreenHouse from '../../Assets/greenhouse.png'
+import GreenHouse from '../../Assets/greenhouse.gif'
 
 function Greenhouse(props) {
   return (
     <Layout>
       <div className='content-container'>
-        <h2>Tablet View</h2>
+        <h2 className='project-title'>Tablet View</h2>
         <img className='project-image' src={GreenHouse} />
+        <h2 className='project-description-title'>Project Description</h2>
+        <p className='project-description'>Project Description with some text should go in this location</p>
+        <h2>Built With</h2>
+        <p className='tech-stack-fe'>Front End - React, CSS & Styled Components</p>
+        <p className='tech-stack-be'>Back End - Express, MongoDB & Mongoose</p>
         
         
       
